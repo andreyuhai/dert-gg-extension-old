fe7e9aa43ce7d7c96a7fc5a2ceb50fa56fce9f2f
andreyuhai <burakaymakci@gmail.com>
2021-11-14 20:38:07 +0100

    Add UNAUTHORIZED_ICONSET and AUTHORIZED_ICONSET constants

--- background.js
+++ background.js
@@ -1,3 +1,23 @@
+const UNAUTHORIZED_ICONSET = {
+  "16": "/img/droplet_unauthenticated_16.png",
+  "24": "/img/droplet_unauthenticated_24.png",
+  "32": "/img/droplet_unauthenticated_32.png",
+  "64": "/img/droplet_unauthenticated_64.png",
+  "128": "/img/droplet_unauthenticated_128.png",
+  "256": "/img/droplet_unauthenticated_256.png",
+  "512": "/img/droplet_unauthenticated_512.png"
+}
+
+const AUTHORIZED_ICONSET = {
+  "16": "/img/droplet_16.png",
+  "24": "/img/droplet_24.png",
+  "32": "/img/droplet_32.png",
+  "64": "/img/droplet_64.png",
+  "128": "/img/droplet_128.png",
+  "256": "/img/droplet_256.png",
+  "512": "/img/droplet_512.png"
+}
+
 let buttonName = "derdini sikeyim";
 
 chrome.runtime.onInstalled.addListener(() => {
